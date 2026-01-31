c1560c105e6e0d1a0206a43a4b05d5d2fa302b67
tan1abhi <abhishektan1704@gmail.com>
2026-01-31 11:27:11 +0000

    added apiClientConfig

--- portfolio_backend/src/main/java/org/example/portfolio_backend/apiController/ApiClientController.java
+++ portfolio_backend/src/main/java/org/example/portfolio_backend/apiController/ApiClientController.java
@@ -1,13 +1,53 @@
 package org.example.portfolio_backend.apiController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.portfolio_backend.model.DataSender;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApiClientController {
-    @GetMapping("/hello")
-    public String test(){
-        return "hello";
+    @GetMapping("/investments")
+    public String getInvestments(){
+        return "investment list";
+    }
+
+    @GetMapping("/investments/{id}")
+    public String getInvestmentsById(@PathVariable long id){
+        return "Investment by ID";
+    }
+
+    @GetMapping("/investments/{sector}")
+    public String getInvestmentsBySector(@PathVariable String sector){
+        return "Investmet by sector";
+    }
+
+    @GetMapping("/investments/{type}")
+    public String getInvestmentsByType(@PathVariable String type){
+        return "Investment by type";
+    }
+
+    @GetMapping("/investments/{risk}")
+    public String getInvestmentsByRisk(@PathVariable String risk){
+        return "Investment by risk";
+    }
+
+    @GetMapping("/investments/{curr}")
+    public String getInvestmentsByCurr(@PathVariable String curr){
+        return "Investment by curr";
+    }
+
+    @PostMapping("/investment/addnew")
+    public void addNewInvestment(){
+        return;
+    }
+
+    @PutMapping("/investment/{id}")
+    public void editInvestmentById(@PathVariable long id){
+        return;
+    }
+
+    @DeleteMapping("/investment/{id}")
+    public void deleteInvestmentById(@PathVariable long id){
+        return;
     }
 
 }
